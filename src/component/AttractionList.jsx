85454010eab410408239c8711ac9309ef10063e5
vanessalaurel <vlaurel04@gmail.com>
2025-11-14 17:05:05 +0800

    dumyy

--- src/component/AttractionList.jsx
+++ src/component/AttractionList.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./AttractionList.css";
 
-const PLACEHOLDER_IMG = "https://via.placeholder.com/250x150?text=No+Image";
+const PLACEHOLDER_IMG =
+  "https://images.unsplash.com/photo-1730819176753-3a283f6310bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxob25nJTIwa29uZyUyMGxhbnRhdSUyMGJ1ZGRoYXxlbnwxfHx8fDE3NjIzMjU2ODh8MA&ixlib=rb-4.1.0&q=80&w=1080";
 
 const AttractionList = ({ attractions }) => {
   if (!attractions || attractions.length === 0)
